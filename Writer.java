b223e55f5d7f173de6e1241dc4b03e790b533a85
shahd181 <120758944+shahd181@users.noreply.github.com>
2022-12-17 22:48:40 +0200

    solve starvation
    Solving Starvation by adding new semaphores "entry_mutex" and "out_mutex"

--- Writer.java
+++ Writer.java
@@ -20,15 +20,40 @@ public class Writer implements Runnable {
 public void run () {
     
          try {
-                MainFrame.rw_mutex.acquire();
+           MainFrame.entry_mutex.acquire();   //  this has same significance as in above
+             
+             MainFrame.out_mutex.acquire();
+             //  this ensures that out_mutex is not being changed while it executes
+             
+             if(MainFrame.in_count == MainFrame.out_count) {
+                 // means no reader is currently executing in the critical section
+                 // so it simply releases the out_mutex and enters the critical section
+                 
+                 MainFrame.out_mutex.release();
+                 
+             }
+             else {
+                 // means some reader process is in critical section
+                 
+                 MainFrame.writer_waiting = true;  // the writer process would have to wait
+                 MainFrame.out_mutex.release();   // this was only to ensure sync of out_count
+                 
+                 MainFrame.rw_mutex.acquire();
+                 // now the writer process enters the blockedQueue of the rw_semaphore
+                 // after acquiring it, the proces is now ready to enter the critical section
+                 
+                 MainFrame.writer_waiting = false;
+             }
+             
+                // ***** CRITICAL SECTION ***** // 
              
              MainFrame.resources = MainFrame.resources - 1 ;
-             
-             // ***** CRITICAL SECTION ***** // 
-              System.out.println("writer"+ "  : "+ MainFrame.resources );
+             System.out.println("writer"+ "  : "+ MainFrame.resources );
                 MainFrame.jTextArea1.append("Seat reserved by Customer : " + Thread.currentThread().getName() + "             Seats Number Now is:    " + resources +"\n");
- 
-              MainFrame.rw_mutex.release();
+         
+             
+        
+            MainFrame.entry_mutex.release();
          } catch (InterruptedException ex) {
              Logger.getLogger(Writer.class.getName()).log(Level.SEVERE, null, ex);
          }
